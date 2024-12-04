015d53adaa882e97d038772d4809d3647117d91e
Krzysztof Kuzara <krzysztof.kzr1@gmail.com>
2024-12-04 22:11:22 +0100

    fix: main package.json scripts

--- apps/billing-api-nestjs/src/upstash/upstash.guard.ts
+++ apps/billing-api-nestjs/src/upstash/upstash.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { CanActivate, Injectable } from "@nestjs/common";
 import { Observable } from "rxjs";
 
 @Injectable()
